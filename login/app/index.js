6e1498c56b9835bad6d9f612e42e5f3be88e1ca2
expertup <piotr.siekierzynski@gmail.com>
2023-04-25 14:49:43 +0200

    Delete login directory

--- login/app/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function Login_register(){
-  return(
-    <div>login</div>
-  )
-}
-
-export default Login_register;
--- login/client/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { hydrateRoot } from 'react-dom/client';
-import { loadableReady } from '@loadable/component';
-
-import Login_register from '../app/index.js';
-
-loadableReady(() => {
-  hydrateRoot(document.getElementById('root'), <Login_register />);
-});
